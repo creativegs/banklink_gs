85019729608667946e70cde75585c1569a992496
Oļegs Boldirevs <olegs.boldirevs@gmail.com>
2019-09-19 14:21:41 +0300

    Bump version

--- lib/banklink/swedbank.rb
+++ lib/banklink/swedbank.rb
@@ -6,6 +6,7 @@ module Banklink
 
     # Raw X509 certificate of the bank, string format.
     mattr_accessor :bank_certificate
+
     # RSA public key of the bank, taken from the X509 certificate of the bank. OpenSSL container.
     def self.get_bank_public_key
       cert = self.bank_certificate
@@ -14,6 +15,7 @@ module Banklink
 
     mattr_accessor :private_key
     mattr_accessor :pem_pass
+
     # Our RSA private key. OpenSSL container.
     def self.get_private_key
       private_key = self.private_key
